57698822900707d2f5a8c8ce3846de4b72150d28
Giacomo Gorgerino <giacomo.gorge@gmail.com>
2026-02-01 10:05:28 +0100

    change in init file

--- components/Sainlogic_WeatherStation/__init__.py
+++ components/Sainlogic_WeatherStation/__init__.py
@@ -2,14 +2,18 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
-# Definiamo il namespace e la classe C++
-meteo_ns = cg.esphome_ns.namespace('Sainlogic_WeatherStation')
-MeteoStation = meteo_ns.class_('MeteoStation', cg.PollingComponent)
+# Namespace C++ (deve corrispondere a quello che useresti in C++)
+sainlogic_ns = cg.esphome_ns.namespace('sainlogic_weather_station')
+# Classe C++
+SainlogicStation = sainlogic_ns.class_('MeteoStation', cg.PollingComponent)
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(MeteoStation),
+    cv.GenerateID(): cv.declare_id(SainlogicStation),
 }).extend(cv.polling_component_schema('60s'))
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    
+    # FORZA l'inclusione del file header rinominato
+    cg.add_global(cg.RawStatement('#include "Sainlogic_WeatherStation.h"'))
