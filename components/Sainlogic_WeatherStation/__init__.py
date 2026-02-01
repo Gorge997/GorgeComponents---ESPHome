0cb5a1137e51e5f3204e223bf2e017d9f3d75afb
Giacomo Gorgerino <giacomo.gorge@gmail.com>
2026-02-01 10:22:56 +0100

    Fix

--- components/Sainlogic_WeatherStation/__init__.py
+++ components/Sainlogic_WeatherStation/__init__.py
@@ -2,9 +2,8 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
-# Namespace C++ (deve corrispondere a quello che useresti in C++)
-sainlogic_ns = cg.esphome_ns.namespace('sainlogic_weatherstation')
-# Classe C++
+# Definiamo il namespace e la classe
+sainlogic_ns = cg.esphome_ns.namespace('sainlogic_weather_station')
 SainlogicStation = sainlogic_ns.class_('MeteoStation', cg.PollingComponent)
 
 CONFIG_SCHEMA = cv.Schema({
@@ -14,6 +13,5 @@ CONFIG_SCHEMA = cv.Schema({
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    
-    # FORZA l'inclusione del file header rinominato
+    # Il nome del file deve essere quello che hai sul repo
     cg.add_global(cg.RawStatement('#include "Sainlogic_WeatherStation.h"'))
--- components/Sainlogic_WeatherStation/sensor.py
+++ components/Sainlogic_WeatherStation/sensor.py
@@ -2,13 +2,18 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import (
-    UNIT_CELSIUS, UNIT_PERCENT, UNIT_DEGREES,
-    STATE_CLASS_MEASUREMENT, STATE_CLASS_TOTAL_INCREASING,
-    DEVICE_CLASS_TEMPERATURE, DEVICE_CLASS_HUMIDITY, DEVICE_CLASS_ILLUMINANCE
+    UNIT_CELSIUS, 
+    UNIT_PERCENT, 
+    UNIT_DEGREES, 
+    STATE_CLASS_MEASUREMENT, 
+    STATE_CLASS_TOTAL_INCREASING,
+    DEVICE_CLASS_TEMPERATURE, 
+    DEVICE_CLASS_HUMIDITY, 
+    DEVICE_CLASS_ILLUMINANCE
 )
-from . import MeteoStation, meteo_ns
+# IMPORTANTE: I nomi qui devono corrispondere a __init__.py
+from . import SainlogicStation, sainlogic_ns 
 
-# Mappa dei sensori con le loro proprietà
 TYPES = {
     "wind_direction":  sensor.sensor_schema(unit_of_measurement=UNIT_DEGREES, icon="mdi:compass", accuracy_decimals=0),
     "wind_avg":        sensor.sensor_schema(unit_of_measurement="m/s", icon="mdi:weather-windy", accuracy_decimals=1),
@@ -22,12 +27,12 @@ TYPES = {
 }
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(meteo_ns): cv.use_id(MeteoStation),
+    cv.GenerateID(sainlogic_ns): cv.use_id(SainlogicStation),
     **{cv.Optional(t): TYPES[t] for t in TYPES}
 })
 
 async def to_code(config):
-    parent = await cg.get_variable(config[meteo_ns])
+    parent = await cg.get_variable(config[sainlogic_ns])
     for key in TYPES:
         if key in config:
             sens = await sensor.new_sensor(config[key])
